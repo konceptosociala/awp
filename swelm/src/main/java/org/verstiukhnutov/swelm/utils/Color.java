78e1a9136d36ff461905c4995bd667918669c238
TeksPeks <verstyukm@gmail.com>
2024-04-08 15:57:08 +0300

    Add app colors to `Color` class
    Also add toHex method

--- swelm/src/main/java/org/verstiukhnutov/swelm/utils/Color.java
+++ swelm/src/main/java/org/verstiukhnutov/swelm/utils/Color.java
@@ -1,9 +1,16 @@
 package org.verstiukhnutov.swelm.utils;
 
 public class Color extends java.awt.Color {
+    public static final Color GRAY = new Color(57, 57, 57);
+    public static final Color LIGHT_GRAY = new Color(71, 71, 71);
+    public static final Color BLUE = new Color(46, 138, 247);
+
 
     public Color(int r, int g, int b) {
         super(r, g, b);
     }
 
+    public String toHex() {
+        return String.format("#%02x%02x%02x", getRed(), getGreen(), getBlue());
+    }
 }
